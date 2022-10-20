2b0543d8d3b7dab1ed275b677e05caa9582e654c
terrnit <terrnit.megaman@gmail.com>
2022-10-20 07:29:36 +0500

    refactor(contact-/-footer): fixes some visual bug / clear code-base

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./contact.css";
 import Logo from "../../images/logo_white.png";
 import Form from "../ui-components/form/form.jsx";
+import Footer from "../footer/footer.jsx";
+
 const contact = () => {
   return (
     <div className="contact__container">
@@ -10,26 +12,7 @@ const contact = () => {
         <h3 className="contact__heading">Contact Us</h3>
         <Form />
       </div>
-      <div className="footer__container">
-        <div className="link__container">
-          <a href="">
-            <h3>Donate</h3>
-          </a>
-
-          <a href="">
-            <h3>Github</h3>
-          </a>
-
-          <a href="">
-            <h3>VK</h3>
-          </a>
-
-          <a href="">
-            <h3>Privacy Policy</h3>
-          </a>
-        </div>
-        <h3 className="footer__copyright">Copyright by ShokoTeam</h3>
-      </div>
+      <Footer />
     </div>
   );
 };
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Parallax } from "react-parallax";
+import Logo from "../../images/logo_white.png";
+import Arrow from "../../images/icons/ArrowDown.svg";
+import "./hero.css";
 
 const hero = () => {
   return (
@@ -7,9 +10,16 @@ const hero = () => {
       <Parallax
         bgImage={require("../../images/Bongo-Jams.gif")}
         bgImageAlt="Not found"
-        strength={400}
+        strength={600}
       >
-        <div style={{ height: 1000 }}></div>
+        <div className="hero__container">
+          <img src={Logo} alt="shoko" className="hero__logo" />
+          <div className="hero__content">
+            <h1 className="hero__heading">Shoko</h1>
+            <h3 className="hero__desription">App development, and other</h3>
+          </div>
+          <img src={Arrow} alt="arrow" className="hero__arrow" />
+        </div>
       </Parallax>
     </div>
   );
