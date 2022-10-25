423e0ae628fdd6efb1d2c2ce115d263ce6e97515
terrnit <terrnit.megaman@gmail.com>
2022-10-26 01:03:16 +0500

    style(hero): add full adaptive for hero section

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Parallax } from "react-parallax";
 import Logo from "../../images/logo_white.png";
 import Arrow from "../../images/icons/ArrowDown.svg";
 import "./hero.css";
@@ -7,20 +6,19 @@ import "./hero.css";
 const hero = () => {
   return (
     <div className="wrapper">
-      <Parallax
-        bgImage={require("../../images/Bongo-Jams.gif")}
-        bgImageAlt="Not found"
-        strength={600}
-      >
-        <div className="hero__container">
-          <img src={Logo} alt="shoko" className="hero__logo" />
-          <div className="hero__content">
-            <h1 className="hero__heading">Shoko</h1>
-            <h3 className="hero__desription">App development, and other</h3>
+      <div class="parallax">
+        <div class="parallax-content">
+          <div className="hero__container">
+            <img src={Logo} alt="shoko" className="hero__logo" />
+            <div className="hero__content">
+              <h1 className="hero__heading">Shoko</h1>
+              <h3 className="hero__desription">App development, and other</h3>
+            </div>
+            <img src={Arrow} alt="arrow" className="hero__arrow" />
           </div>
-          <img src={Arrow} alt="arrow" className="hero__arrow" />
         </div>
-      </Parallax>
+        <div class="mask"></div>
+      </div>
     </div>
   );
 };
