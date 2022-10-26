e166e40e387a6a865d27c435d5baef235d67688a
terrnit <terrnit.megaman@gmail.com>
2022-10-26 05:35:11 +0500

    style(team): add gradient animation / delete scrollbar

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -11,16 +11,16 @@ const team = () => {
   return (
     <div className="team__container">
       <div className="team__wrapper">
-        <div className="team__content">
-          <h3 className="team__sub">
+        <div className="team__content gradient">
+          <h3 className="team__sub ">
             We are ordinary people, who are united by common interests and as
             well as the desire to improve ourselvers. As we are young developer,
             we still have a lot to achieve
           </h3>
-          <h3 className="team__description">
-            We don't know where thsi path would lead us to but we hope to
+          <p className="team__description">
+            We don't know where this path would lead us to but we hope to
             achieve a lot
-          </h3>
+          </p>
         </div>
 
         <h1 className="team__heading">Our Team</h1>
