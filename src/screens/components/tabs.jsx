de6258156856a64fb2f6c137ab5d7088496dfe70
terrnit <terrnit.megaman@gmail.com>
2022-11-30 20:29:36 +0500

    feat(foresprivacy): add privacy for projects / add favicon's

--- src/screens/components/tabs.jsx
+++ src/screens/components/tabs.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { Box, Flex, TabList, TabPanel, TabPanels, Tabs, Tooltip } from "@chakra-ui/react";
 import CustomTab from '../../components/works/tabs-motion';
 import AnimHeading from '../../components/atoms/anim-heading/AnimHeading';
-import AnimText from '../../components/atoms/anim-text/AnimText';
 import ComingSoon from '../../components/works/components/coming-soon';
 import Homepage from "./project/homepage.jsx"
+import Fores from './project/fores';
 
 
 const PrivacyTabs = () => {
@@ -59,11 +59,9 @@ const PrivacyTabs = () => {
           <TabPanel>
             <Box mt={5}>
               <AnimHeading alignText="center" custom={2}>
-                Hello
+                Политика конфиденциальности для мобильного приложения "Fores"
               </AnimHeading>
-              <AnimText custom={3}>
-                Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
-              </AnimText>
+              <Fores />
             </Box>
           </TabPanel>
           <TabPanel>
